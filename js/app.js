1570a580d8438ba472b5d185185018e90987c603
Alexi Carey <ciegoproductions@gmail.com>
2017-03-14 18:52:18 -0600

    Add homer css and js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
-	
-	
-	
+
+	element = document.getElementById("trigger");
+	microwave = document.getElementById("microwave");
+	homer = document.getElementById("homer-wrapper");
+
+	element.addEventListener("click", function (e) {
+	    e.preventDefault;
+	    microwave.classList.remove("animated");
+	    microwave.offsetWidth = microwave.offsetWidth;
+	    microwave.classList.add("animated");
+	    $(homer).animate({"width":"650px"}, 1200);
+	    $(homer).animate({"width":"165px"}, 1200);
+	}, false);
+
 });
